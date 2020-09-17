31ad7cd20386d49ed9953ebe8331177c7270864a
Alexandar <alexandar1000@gmail.com>
2020-09-17 22:52:23 +0100

    Add recurring temp reading

--- mayflower-webplatform/src/Components/CurrentOverview/Thermometer.js
+++ mayflower-webplatform/src/Components/CurrentOverview/Thermometer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { withStyles } from '@material-ui/core/styles';
 
 import { 
@@ -14,6 +15,31 @@ const styles = theme => ({
   });
 
 class Thermometer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { temperature: 0, interval: null };
+      }
+
+    getLatestTemperature() {
+        axios.get(`http://localhost:8000/api/v1/temperatures/0/`)
+          .then(res => {
+              this.setState({
+                  temperature:  (Math.round(res.data.temperature * 100) / 100).toFixed(1)
+              })
+          })
+    }
+    componentDidMount() {
+        this.getLatestTemperature();
+        this.setState({
+            interval: setInterval(() => {
+                this.getLatestTemperature()
+              }, 1000)})
+      }
+
+    componentWillUnmount() {
+        clearInterval(this.state.interval);
+    }
+
     render() {
         const {classes} = this.props;
         return (
@@ -23,7 +49,7 @@ class Thermometer extends React.Component {
                 />
                 <CardContent>
                     <Typography>
-                        25.3c
+                        {this.state.temperature}c
                     </Typography>
                 </CardContent>
             </Card>
